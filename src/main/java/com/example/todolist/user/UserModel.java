02b80265d77ff09ebecd829f9aae5796102116a2
Mateus <mateusc.ramos@outlook.com>
2023-10-09 21:04:11 -0300

    first commit

---
+++ src/main/java/com/example/todolist/user/UserModel.java
@@ -0,0 +1,39 @@
+package com.example.todolist.user;
+
+public class UserModel {
+    
+    private String name;
+    private String username;
+    private String password;
+
+    public UserModel(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
