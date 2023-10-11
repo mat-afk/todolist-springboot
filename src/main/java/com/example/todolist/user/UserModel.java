ac38d6cdc921478abf550dc8ad2c03bae4653b04
Mateus <mateusc.ramos@outlook.com>
2023-10-11 02:39:27 -0300

    jpa e integracao com jdbc

--- src/main/java/com/example/todolist/user/UserModel.java
+++ src/main/java/com/example/todolist/user/UserModel.java
@@ -1,39 +1,31 @@
 package com.example.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-    
-    private String name;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
+
+    private String name;
+    
     private String password;
 
-    public UserModel(String username, String name, String password) {
-        this.username = username;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
